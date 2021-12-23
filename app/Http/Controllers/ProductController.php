9701e9b6de8cb1b3194841d43a5a250793ad0ef9
SerpensHell <katyazabelina@mail.ru>
2021-12-23 15:25:03 +0300

    3 лаба

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Illuminate\Routing\Controller;
+
+Class ProductController extends Controller
+{
+    public function list (): array
+    {
+        return [];
+    }
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'materialize-css/dist/js/materialize.min'
+import axios from "axios";
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios.create({
+    baseURL: 'http://localhost/api'
+});
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
